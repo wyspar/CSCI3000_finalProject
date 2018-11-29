d5449ea8473af252696f2c77300021404b37a5ec
Douglas Richardson (DRRICH7754) <DRRICH7754@ung.edu>
2018-11-29 09:18:50 -0500

    update1

---
+++ Richardson_finalProject.js
@@ -0,0 +1,5 @@
+/*
+Name: Douglas Richardson
+Date: 11/1/18
+Desc: CSCI 3000 DA C0. Final Project. Home page js
+*/
---
+++ main_finalProject.php
@@ -0,0 +1,3 @@
+<?php
+include_once('P:\CSCI web programming\csci3000_finalProject\Richardson_finalProject.html');
+?>
