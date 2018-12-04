2f01c343ef7e2f6c7092a48fd2a3764c345a6e45
MinayaA <42653036+MinayaA@users.noreply.github.com>
2018-12-03 22:37:25 -0500

    Update Richardson_finalProject.js

--- Richardson_finalProject.js
+++ Richardson_finalProject.js
@@ -4,11 +4,11 @@ Date: 11/1/18
 Desc: CSCI 3000 DA C0. Final Project. Home page js
 */
 function locationPage(){
-	window.location.href = 'Richardson_finalProject_locations.html';
+	window.location.href = 'Richardson_finalProject_locations.php';
 }
 function homePage(){
-	window.location.href = 'Richardson_finalProject.html';
+	window.location.href = 'Richardson_finalProject.php';
 }
 function drinksPage(){
-	window.location.href = 'Richardson_finalProject_drinks.html';
+	window.location.href = 'Richardson_finalProject_drinks.php';
 }
